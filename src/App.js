61b2f49c444f6388a082c9c80b24ea08403bb41f
Le-Mon7 <igorejkee07@gmail.com>
2022-05-31 13:03:48 +0700

    test app #2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home/Home"
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
-  );
-}
+	return (
+		<div>
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</div>
+	);
+};
 
 export default App;
+
+// test 2
