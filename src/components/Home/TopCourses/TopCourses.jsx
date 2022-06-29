5e920d606465d5e4a01c340dc73e8f68a3f445e0
unknown <web-supsu@yandex.ru>
2022-06-29 17:27:14 +0700

    Завершил создание секции Top Courses. Вывел через API топ курсы на главной странице.

--- src/components/Home/TopCourses/TopCourses.jsx
+++ src/components/Home/TopCourses/TopCourses.jsx
@@ -2,10 +2,8 @@ import styles from "./TopCourses.module.scss";
 
 const TopCourses = (props) => {
   return (
-    <section className={styles.main}>
-      <div>
-        <div>
-          <div>
+    <section className={styles.section}>
+      <div className={styles.container}>
             {props.data.text.map((text) => (
               <div className={styles.text}>
                 <span>{text.span}</span>
@@ -13,9 +11,48 @@ const TopCourses = (props) => {
                 <p>{text.p}</p>
               </div>
             ))}
-          </div>
+        <div className={styles.row}>
+          {props.data.cours.map(
+            (elem) =>
+              elem.top && (
+                <div className={styles.blockCard}>
+                  <div className={styles.card}>
+                    <div className={styles.mainImg}>
+                      <a href="/details">
+                        <img className={styles.img} src={elem.img} />
+                      </a>
+                    </div>
+                    <div className={styles.blockText}>
+                      <div className={styles.textInBlock}>
+                        <span>{elem.price}</span>
+                      </div>
+                      <div className={styles.tag}>
+                        <a href="/details">{elem.tag}</a>
+                      </div>
+                      <h3 className={styles.h3}>
+                        <a href="/details">{elem.h3}</a>
+                      </h3>
+                      <p className={styles.p}>{elem.p}</p>
+                      <div className={styles.blockTutor}>
+                        <div className={styles.tutorImg}>
+                          <a href="/details">
+                            <img src={elem.tutorImg} />
+                            {elem.tutor}
+                          </a>
+                        </div>
+                        <div className={styles.lessons}>
+                          <a>
+                            <img src={elem.svg} />
+                            { elem.lessons.replace("Lessons", "") }
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+          )}
         </div>
-        <div className={styles.list}> </div>
       </div>
     </section>
   );
